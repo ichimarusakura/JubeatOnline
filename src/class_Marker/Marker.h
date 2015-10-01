771c35b673d8b9ba250497f94f9cdffa9fe484c8
wgprojects2013 <pastfantasy1640@gmail.com>
2015-10-01 10:34:40 +0900

    c_ImageSequenceというシーケンス画像を扱うクラスと、c_Markerというマーカー関連を扱うクラスを用意しました。未完成です。

---
+++ src/class_Marker/Marker.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef JUBEAT_ONLINE_MARKER_HEADER
+#define JUBEAT_ONLINE_MARKER_HEADER
+
+namespace jubeatOnline {
+
+	/// <summary>マーカーの管理を行いやすくする関数
+	/// ただし、このクラスで扱うのは特定のマーカーであり、プレイ時に有用なクラスである。</summary>
+	class Marker {
+	private:
+
+	public:
+		Marker();
+		~Marker();
+	};
+
+}
+
+
+#endif
